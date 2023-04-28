8c84759a5cc592f28c3ef6871e717bded652a003
jcssilva4 <jcss4@cin.ufpe.br>
2023-04-28 01:30:19 -0300

    user management script

--- user_management/update_sheet.py
+++ user_management/update_sheet.py
@@ -51,32 +51,3 @@ if len(new_users)>0:
 
 else:
 	print('no new users to add')
-'''
-# get job data
-log_dict = dict([])
-cols = []
-firstLine = True
-with os.popen('sacct --allusers --parsable --delimiter='','' --format State,JobID,Start,Elapsed,End,Partition --starttime 1970-01-01T0:00:00') as f:
-	try:
-		for line in f:
-			new_line = line.replace('\n','')
-			new_line = new_line.split(',')
-			#print(new_line)
-			#print(len(new_line))
-			for col in range(len(new_line)-1):
-				if firstLine: # initialize cols
-					log_dict[new_line[col]] = []
-					cols.append(new_line[col])
-				else:
-					log_dict[cols[col]].append(new_line[col])
-			firstLine=False
-	except:
-		print("G")
-
-# update the worksheet
-log_df = pd.DataFrame.from_dict(log_dict)
-worksheet = sheet.get_worksheet(0)
-worksheet.update([log_df.columns.values.tolist()] + log_df.values.tolist())
-
-print('log worksheet updated!')
-'''
