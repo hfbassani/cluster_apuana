4839e8c1b86181b1ab94695a0552e19091a9e120
paulaperazzo <mpps@cin.ufpe.br>
2024-03-26 11:16:19 -0300

    feat/gpu-logs-csv: paramiko test

--- usage_monitor/update_gpu_csv.py
+++ usage_monitor/update_gpu_csv.py
@@ -1,8 +1,9 @@
-import subprocess
+#import subprocess
 import csv
 from dotenv import load_dotenv
 import os
 from datetime import datetime
+import paramiko
 
 load_dotenv()
 
@@ -22,9 +23,6 @@ nodes_ips = [
 user = os.getenv('SSH_USER')  # get user from .env file
 password = os.getenv('SSH_PASSWORD')  # get password from .env file
 
-print(user)
-print(password)
-
 # .csv file name
 output_csv = './monitor/gpu_log.csv'
 
@@ -41,19 +39,21 @@ with open(output_csv, 'a', newline='') as csvfile:
     for ip in nodes_ips:
         try:
             # ssh command to access the node and get data
-            comando_ssh = f'sshpass -p "{password}" ssh -o StrictHostKeyChecking=no {user}@{ip} "nvidia-smi --format=csv,noheader --query-gpu=index,name,temperature.gpu,memory.used"'
-            print(f'Conectando ao nó {ip}...')
+            ssh_client = paramiko.SSHClient()
+            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh_client.connect(ip, username=user, password=password)
+
+            print('connected to node ', ip)
             
-            # execute ssh command
-            output = subprocess.check_output(comando_ssh, shell=True, stderr=subprocess.STDOUT).decode()
-            print('saida ', output)
+            # Executar o comando nvidia-smi
+            stdin, stdout, stderr = ssh_client.exec_command('nvidia-smi --format=csv,noheader --query-gpu=index,name,temperature.gpu,memory.used')
 
             # get time and hostname
             time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
             hostname = ip.split('.')[0]
             
             # read the output and write it to the csv file
-            for line in output.strip().split('\n'):
+            for line in stdout:
                 index, name, temperature, memory = line.strip().split(',')
                 writer.writerow({'index': index, 'name': name, 'temperature_gpu': temperature, 'memory_used': memory, 'hostname': hostname, 'time': time})
         except Exception as e:
