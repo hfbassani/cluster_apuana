c80a1be5c6ecb141748fc674d5b9103f613370fa
paulaperazzo <mpps@cin.ufpe.br>
2024-05-02 18:29:35 -0300

    feat/database-connection: job logs added to database

--- usage_monitor/update_gpu_csv.py
+++ usage_monitor/update_gpu_csv.py
@@ -1,7 +1,7 @@
 #import subprocess
 from dotenv import load_dotenv
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import paramiko
 from database import DatabaseConnection
 
@@ -31,10 +31,12 @@ nodes_ips = [
     'cluster-node10.cin.ufpe.br'
 ]
 
+manager_ip = ['slurm-manager1.cin.ufpe.br']
+
 user = os.getenv('SSH_USER')  # get user from .env file
 password = os.getenv('SSH_PASSWORD')  # get password from .env file
 
-# connect to each node with ssh 
+### connect to each node with ssh ###
 for ip in nodes_ips:
     try:
         # ssh command to access the node and get data
@@ -98,27 +100,71 @@ for ip in nodes_ips:
         print(f'Erro ao conectar ao nó {ip}: {str(e)}')
 
 ### get the queue info ###
-stdin, stdout, stderr = ssh_client.exec_command('squeue --Format=JobID,Name,UserName,State,TimeUsed,NodeList')
-stdout.channel.recv_exit_status()
-time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+try:
+    stdin, stdout, stderr = ssh_client.exec_command('squeue --Format=JobID,Name,UserName,State,TimeUsed,NodeList')
+    stdout.channel.recv_exit_status()
+    time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
-next(stdout)
+    next(stdout)
 
-for line in stdout:
-    fields = line.strip().split()
-    job_id, name, user, state, time_user, nodelist = fields
-    job_id = int(job_id)
-    
-    db.cursor().execute(
-        'INSERT INTO queue (jobid, name, "USER", state, time, nodelist, last_updated) '
-        "VALUES (%s, %s, %s, %s, %s, %s, %s)",
-        (job_id, name, user, state, time_user, nodelist, time)
-    )
+    for line in stdout:
+        fields = line.strip().split()
+        job_id, name, user, state, time_user, nodelist = fields
+        job_id = int(job_id)
+        
+        db.cursor().execute(
+            'INSERT INTO queue (jobid, name, "USER", state, time, nodelist, last_updated) '
+            "VALUES (%s, %s, %s, %s, %s, %s, %s)",
+            (job_id, name, user, state, time_user, nodelist, time)
+        )
 
-    db.commit()
+        db.commit()
+
+    print('queue info saved!')
+
+except Exception as e:
+    print(f'Error getting queue information: {str(e)}')
 
 ssh_client.close()
 
-db.close() # close the connection
+### get job logs info ###
+try:
+    current_date = datetime.now().date()
+    zero_time = datetime.combine(current_date, datetime.min.time())
+    zero_time = zero_time.strftime('%Y-%m-%dT%H:%M:%S')
 
-print('gpu state saved!')
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(manager_ip[0], username=user, password=password)
+    print('connected to manager')
+
+    stdin, stdout, stderr = ssh_client.exec_command('sacct --allusers --parsable --delimiter='','' --format State,JobID,Submit,Start,End,Elapsed,Partition,ReqCPUS,ReqMem,ReqTRES --starttime 2024-01-01T0:00:00')
+    next(stdout)
+
+    for line in stdout:
+        fields = line.strip().split(',')
+
+        for f in range(len(fields)):
+            if fields[f] == None or fields[f] == 'None':
+                fields[f] = zero_time
+
+        state, job_id, submit, start, end, elapsed, partition, req_cpus, req_mem, req_gpu = fields[:10]
+        state = state.split(" ")[0]
+        job_id = job_id.split('_')[0]
+        req_mem = req_mem.replace('M', '').replace('G', '')
+        req_gpu = req_gpu.replace('billing=', '')
+
+        db.cursor().execute(
+            'INSERT INTO job_log (state, jobid, submit, start, "End", elapsed, partition, reqcpus, reqmem, reqgpu) '
+            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            (state, job_id, submit, start, end, elapsed, partition, req_cpus, req_mem, req_gpu)
+        )
+
+        db.commit()
+        print('job info saved!')
+
+except Exception as e:
+    print(f'Error connecting to manager: {str(e)}')
+
+ssh_client.close()
+db.close() # close the connection
